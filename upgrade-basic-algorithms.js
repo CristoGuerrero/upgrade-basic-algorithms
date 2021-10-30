c530c64fe296f0d90b73ae25202cc041a72d18c7
CristoGuerrero <92631968+CristoGuerrero@users.noreply.github.com>
2021-10-30 14:53:23 +0200

    prueba desde web

--- upgrade-basic-algorithms.js
+++ upgrade-basic-algorithms.js
@@ -35,3 +35,5 @@ console.log(fullprice)
 let globalBasePrice = 10000;
 const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+prueba enviada desde web
